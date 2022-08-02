7fe4275e08a4dd61ae26f3613777c61cdecd6694
Denis <drop3x25@gmail.com>
2022-08-02 20:56:18 +0300

    data input fixes

--- Medical Insurance Charges Regression/WebApp.py
+++ Medical Insurance Charges Regression/WebApp.py
@@ -6,38 +6,38 @@ from predict import predict_charge
 def main():
     st.title("Medical Insurance Price Prediction In USA")
 
-    age = st.slider("Select your age:", 18, 100)
+    age = st.slider("Select your age", 18, 100)
     bmi = st.text_input("Your BMI Index")
     children = st.text_input("Number of children covered by health insurance")
-    male_female = st.selectbox("Pick your sex", ["male", "female"])
+    male_female = st.selectbox("Sex", ["Male", "Female"])
     smoking = st.selectbox("Do you smoke?", ["Yes", "No"])
     region = st.selectbox("What region of USA do you live in?", ["Southeast", "Southwest",
                                                                  "Northeast", "Northwest"])
 
     # Converting Categorical Values To Numeric,i.e. data preprocessing
-    if male_female == "male":
+    if male_female == "Male":
         male_female = 1
     else:
         male_female = 0
 
-    if smoking == "yes":
+    if smoking == "Yes":
         smoking = 1
     else:
         smoking = 0
 
     regions_dict = {"Northwest": 0, "Southeast": 0, "Southwest": 0}
-    for key in regions_dict.items():
+    for key, value in regions_dict.items():
         if key == region:
             regions_dict[key] = 1
 
     prediction = ""
 
     if st.button("Predict Price"):
-        data = {"age": [int(age)], "bmi": [float(bmi)], "children": [children], "sex_male": [male_female],
-                "smoker_yes": [int(smoking)],
+        data = {"age": [age], "bmi": [round(float(bmi), 2)], "children": [children], "sex_male": [male_female],
+                "smoker_yes": [smoking],
                 "region_northwest": [regions_dict["Northwest"]],
                 "region_southeast": [regions_dict["Southeast"]], "region_southwest": [regions_dict["Southwest"]]}
-        prediction = predict_charge(data)
+        prediction = f"{round(predict_charge(data))}$"
 
     st.success(prediction)
 
