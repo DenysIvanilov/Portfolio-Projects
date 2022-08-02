9f3da0960d9cfed1b89c052092dd32ae0f39a44e
Denis <drop3x25@gmail.com>
2022-08-02 21:56:11 +0300

    fixed path after refactoring

--- MedicalInsuranceChargesRegression/predict.py
+++ MedicalInsuranceChargesRegression/predict.py
@@ -2,8 +2,10 @@ import numpy as np
 import pickle
 import pandas as pd
 
-model_filename = "/NotebookAndModel/trained_model.pkl"
-scaler_filename = "/NotebookAndModel/trained_scaler.pkl"
+model_filename = "C:/Users/suo/Desktop/Portfolio Projects/MedicalInsuranceChargesRegression/" \
+                 "NotebookAndModel/trained_model.pkl"
+scaler_filename = "C:/Users/suo/Desktop/Portfolio Projects/MedicalInsuranceChargesRegression/" \
+                  "NotebookAndModel/trained_scaler.pkl"
 
 loaded_model = pickle.load(open(model_filename, "rb"))
 loaded_scaler = pickle.load(open(scaler_filename, "rb"))
