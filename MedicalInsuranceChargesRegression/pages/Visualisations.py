83772fcb7589ae424b3efffe1fb4e433a87e6e91
Denis <drop3x25@gmail.com>
2022-08-03 15:14:27 +0300

    plots, analysis and visualisations page adjustments

--- MedicalInsuranceChargesRegression/pages/Visualisations.py
+++ MedicalInsuranceChargesRegression/pages/Visualisations.py
@@ -11,6 +11,7 @@ age_smoke = "/app/portfolio-projects/MedicalInsuranceChargesRegression/graphs/ag
 
 def main():
     st.title("Visualisation and EDA of Medical Insurance Charges Data")
+
     st.subheader("Correlation Matrix")
     st.image(image=heatmap)
     st.write("Instantly, smoking is hugely correlated with a price of insurance. "
@@ -19,25 +20,30 @@ def main():
              " This means as either of the values rises, the other one will rise as well. "
              "Let's look at the pairplot.")
     st.write("Note: in the pairplot 0 in smoker represents 'Not a smoker' and 1 represents 'Is a smoker'.")
+
     st.subheader("Pairplot")
     st.image(image=pairplot)
-    st.write("Just as a confirmation, we can see that for lower BMI scores charges are lower. "
-             "Also, we can see prices tilting to the right as age increases.")
+    st.write("Just as a confirmation, we can see that for lower BMI scores charges are lower and around "
+             "BMI = 30 charges start to become higher. This is probably due to BMI Index around 30 meaning obesity.")
     st.write("As expected, charges for smokers are much bigger on average.")
+
     st.subheader("Distribution of Charges")
     st.image(image=dist1)
     st.image(image=dist2)
-    st.write("It seems like women tend to pay less for insurance overall.")
+    st.write("It seems like there is not much of a difference in price between men and women, but"
+             " men do tend to pay more extreme prices more often.")
     st.image(image=dist3)
     st.write("This is a very strong correlation between prices of insurance and being a smoker. Almost everyone who "
              "pays cheaper don't smoke.")
+
     st.subheader("Scatter plots")
     st.image(image=age)
     st.write("Here we can see how age impacts prices.")
     st.image(image=age_smoke)
     st.write("And here is the same plot but for smokers vs non-smokers. Clearly smoking does have a great impact on the"
              " charges.")
-    st.caption("For more analysis and conclusion you can check out whole notebook [here.]"
+
+    st.caption("For more plots, analysis and conclusion you can check out whole notebook [here.]"
                "(https://github.com/DenysIvanilov/Portfolio-Projects/blob/main/"
                "MedicalInsuranceChargesRegression/NotebookAndModel/Medical_Insurance_Charges.ipynb)")
 
