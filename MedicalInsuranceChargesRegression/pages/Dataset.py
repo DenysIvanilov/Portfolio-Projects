bcd0a0758712d4c5f7434ed2308b042e76e19948
Denis <drop3x25@gmail.com>
2022-08-03 11:29:50 +0300

    visualisations/dataset features

--- MedicalInsuranceChargesRegression/pages/Dataset.py
+++ MedicalInsuranceChargesRegression/pages/Dataset.py
@@ -8,6 +8,16 @@ def main():
     st.title("Dataset used for EDA and training the model")
 
     st.dataframe(data=data)
+    st.write(f"age: age of primary beneficiary\n"
+             f"sex: insurance contractor gender, female, male\n"
+             f"bmi: Body mass index, providing an understanding of body, weights that are relatively high or low "
+             f"relative to height, "
+             f"objective index of body weight (kg / m ^ 2) using the ratio of height to weight, ideally 18.5 to 24.9\n"
+             f"children: Number of children covered by health insurance / Number of dependents\n"
+             f"smoker: Smoking\n"
+             f"region: the beneficiary's residential area in the US, northeast, southeast, southwest, northwest.\n"
+             f"charges: Individual medical costs billed by health insurance\n")
+
 
 if __name__ == "__main__":
     main()
--- MedicalInsuranceChargesRegression/pages/Visualisations.py
+++ MedicalInsuranceChargesRegression/pages/Visualisations.py
@@ -1,16 +1,33 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
-heatmap = "/app/portfolio-projects/MedicalInsuranceChargesRegression/graphs/heatmap.png"
+data = pd.read_csv("/app/portfolio-projects/MedicalInsuranceChargesRegression/NotebookAndModel/DATA/insurance.csv")
+# heatmap = "/app/portfolio-projects/MedicalInsuranceChargesRegression/graphs/heatmap.png"
+# **** Encoding Categorical Data For Heatmap ****
+data_encoded = data.copy()
+le = LabelEncoder()
+le.fit(data_encoded.sex.drop_duplicates())
+data_encoded.sex = le.transform(data_encoded.sex)
+le.fit(data_encoded.smoker.drop_duplicates())
+data_encoded.smoker = le.transform(data_encoded.smoker)
+le.fit(data_encoded.region.drop_duplicates())
+data_encoded.region = le.transform(data_encoded.region)
+# *************************************************
+
+heatmap = sns.heatmap(sns.heatmap(data=data_encoded.select_dtypes(include=[np.number]).corr(), annot=True))
 
 
 def main():
     st.title("Visualisation and EDA of Medical Insurance Charges Data")
 
-    st.image(heatmap, caption="Instantly, smoking is hugely correlated with a price of insurance. "
-                              "Also, as we can see we have some positive correlation between age and charges,"
-                              "as well as bmi and charges. "
-                              "This means as either of the values rises,"
-                              "the other one will rise as well. Let's look at the pairplot.")
+    st.pyplot(heatmap, caption="Instantly, smoking is hugely correlated with a price of insurance. "
+                               "Also, as we can see we have some positive correlation between age and charges,"
+                               "as well as bmi and charges. "
+                               "This means as either of the values rises,"
+                               "the other one will rise as well. Let's look at the pairplot.")
 
 
 if __name__ == "__main__":
