6663a1c2b1ae61e55f6464c5ac9be6247b9083b8
Denis <drop3x25@gmail.com>
2022-08-03 10:57:33 +0300

    creating page for visualisations

---
+++ MedicalInsuranceChargesRegression/pages/graph_page.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+heatmap = "/app/portfolio-projects/MedicalInsuranceChargesRegression/graphs/heatmap.png"
+
+
+def main():
+    st.title("Visualisation and EDA of Medical Insurance Charges Data")
+
+    st.image(heatmap, caption="Instantly, smoking is hugely correlated with a price of insurance. "
+                              "Also, as we can see we have some positive correlation between age and charges,"
+                              "as well as bmi and charges. "
+                              "This means as either of the values rises,"
+                              "the other one will rise as well. Let's look at the pairplot.")
